a53bb63fe0eec208b5db5090c30806dca727409d
Murli Mohan <murli.lspl@gmail.com>
2020-04-10 12:14:34 +0530

    v.1.2

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','homeController@index');  
 Route::get('form-view','homeController@formView');
-Route::post('form-submit','homeController@formValidation');
+Route::post('form-submit','homeController@formValidation');  
